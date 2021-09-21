676c58aced8f5b9441b9de611ddaaa5083cdac47
David-Leatham <david@leatham.de>
2021-09-21 13:17:42 +0200

    Adding two monaco editors.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Editor from "@monaco-editor/react";
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorRef : {},
+      value : "function hello() {\n\talert('Hello world!');\n}"
+    }
+  }
+
+  handleEditorDidMount(editor, monaco) {
+    this.setState({editorRef : editor});
+  }
+
+
+  render() {
+    return (
+      <div id="outer">
+        <div id="display-outer">
+          <div id="display-spacing">
+            <div id="display">
+              <Editor
+                defaultLanguage="javascript"
+                defaultValue={this.state.value}
+                onMount={this.handleEditorDidMount.bind(this)}
+                options={{ minimap : {enabled: false} }}
+              />
+            </div>
+          </div>
+        </div>
+        <div id="input-outer">
+          <div id="input-spacing">
+            <div id="input">
+              <Editor
+                defaultLanguage="javascript"
+                defaultValue={this.state.value}
+                onMount={this.handleEditorDidMount.bind(this)}
+                options={{ minimap : {enabled: false} }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
